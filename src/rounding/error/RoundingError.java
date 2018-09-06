108f206ec6f578eaf094486d4fb2951c1220a530
conas1290 <conas1290@7HJL6Y1.Student.UGDSB.ED>
2018-09-06 14:48:32 -0400

    First commit to set up repo

---
+++ src/rounding/error/RoundingError.java
@@ -0,0 +1,16 @@
+/*
+ * Corbin Nash
+ * Started:  06/09/2018 2:41 PM
+ * Finished:   /  / 
+ * and open the template in the editor.
+ */
+package rounding.error;
+public class RoundingError {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
